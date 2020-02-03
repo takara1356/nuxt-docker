05cd60eff2f1408ce212bb4b4ebd361ff43e3045
takara1356 <goblin7283@gmail.com>
2020-02-03 22:44:46 +0900

    qiitaAPIを取得

---
+++ docker/nuxt.js/src/app/pages/qiita/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        Qiita APIへアクセス
+      </h1>
+      <p>Qiita APIから、Nuxt.jsのタグが付けられた投稿を取得してみる</p>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: "qiita-index",
+    async mounted() {
+      console.log(JSON.stringify(await
+        this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js'), true, '   ')
+        )
+      }
+    }
+</script>>
