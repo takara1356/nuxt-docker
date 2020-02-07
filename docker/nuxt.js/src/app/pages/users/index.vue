461cb12b773b3221f13d71e706b1fe20561b1038
takara1356 <goblin7283@gmail.com>
2020-02-07 21:51:11 +0900

    Make form

---
+++ docker/nuxt.js/src/app/pages/users/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input class="input" v-model="name">
+    <button v-on:click="submit">send</button>
+  </div>
+</template>
+
+<script>
+
+const axios = require('axios');
+
+export default {
+  
+  data() {
+      return {
+        name: ''
+      }
+    },
+  methods:{
+    submit: function() {
+      axios.post('http://localhost:3000/users'), {
+        name: this.name
+      }
+    }
+  }
+
+}
+</script>
+
+<style>
+  
+</style>
